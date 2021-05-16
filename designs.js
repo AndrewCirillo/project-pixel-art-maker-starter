4651b5ca60a97a67956019812a0d7774e5768c5e
AndrewCirillo <59982477+AndrewCirillo@users.noreply.github.com>
2021-05-16 13:23:54 +1000

    updates to designs.js and index.html
    max grid size added to html

--- designs.js
+++ designs.js
@@ -1,10 +1,58 @@
-// Select color input
-// Select size input
+//Constants
+const picker = document.getElementById('colorPicker');
+const size = document.getElementById('sizePicker');
+const gridHeight = document.getElementById('inputHeight');
+const gridWidth = document.getElementById('inputWidth');
+const canvas = document.getElementById('pixelCanvas');
 
-// When size is submitted by the user, call makeGrid()
+//Get initial color
+let color = picker.value;
 
-function makeGrid() {
+//Color input Listener
+picker.addEventListener('change', function(){
+    color = picker.value;
+});
 
-// Your code goes here!
+//input listener and action
+pixelCanvas.addEventListener('click', function(clicked) {
+//Get clicked variables
+    if (clicked.target.nodeName === 'TD') {
+        let cell = clicked.target.style;
+        if (cell.backgroundColor != hexToRGB(color)) {
+            cell.backgroundColor = color;
+        }
+        else {
+            cell.backgroundColor = "transparent";
+        }
+    }
+});
 
+
+// Size input Listener and call makeGrid function
+size.addEventListener ('submit', function(submitted) {
+    submitted.preventDefault();
+    let height = gridHeight.value;
+    let width = gridWidth.value;
+    makeGrid(height, width);
+});
+
+//function to make the grid
+function makeGrid(height, width) {
+    canvas.innerHTML = '';
+    for (let row = 0; row < height; row++) {
+        let addRow = pixelCanvas.insertRow(row);
+        for (let col = 0; col < width; col++) {
+            addRow.insertCell(col);
+        }
+    }
 }
+//function to convert hex to RBG so logical comparison of background color and picker color works.
+function hexToRGB(h) {
+    let r = 0, g = 0, b = 0;
+    r = "0x" + h[1] + h[2];
+    g = "0x" + h[3] + h[4];
+    b = "0x" + h[5] + h[6];
+    return "rgb("+ +r + ", " + +g + ", " + +b + ")";
+}
+
+
